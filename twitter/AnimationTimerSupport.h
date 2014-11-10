de40a69df2690e7246e71d5a535b8a5944ff5bb6
ip82 <ip821@yandex.ru>
2014-11-10 13:34:08 +0200

    Refactoring.

--- twitter/AnimationTimerSupport.h
+++ twitter/AnimationTimerSupport.h
@@ -15,14 +15,10 @@ private:
 public:
 	CAnimationTimerSupport()
 	{
-		timeGetDevCaps(&m_tc, sizeof(TIMECAPS));
-		m_wTimerRes = min(max(m_tc.wPeriodMin, TARGET_RESOLUTION), m_tc.wPeriodMax);
-		timeBeginPeriod(m_wTimerRes);
 	}
 
 	~CAnimationTimerSupport()
 	{
-		timeEndPeriod(m_wTimerRes);
 	}
 
 	static void CALLBACK TimerCallback(UINT wTimerID, UINT msg, DWORD dwUser, DWORD dw1, DWORD dw2)
@@ -35,10 +31,15 @@ public:
 	void StopAnimationTimer()
 	{
 		timeKillEvent(m_uiTimerId);
+		timeEndPeriod(m_wTimerRes);
 	}
 
 	void StartAnimationTimer(/*HWND hWnd, */UINT uiInterval)
 	{
+		timeGetDevCaps(&m_tc, sizeof(TIMECAPS));
+		m_wTimerRes = min(max(m_tc.wPeriodMin, TARGET_RESOLUTION), m_tc.wPeriodMax);
+		timeBeginPeriod(m_wTimerRes);
+
 		auto p = static_cast<T*>(this);
 		m_uiTimerId = timeSetEvent(uiInterval, m_wTimerRes, TimerCallback, (DWORD_PTR)p, TIME_ONESHOT);
 	}
